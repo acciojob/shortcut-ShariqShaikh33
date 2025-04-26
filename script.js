6d922df68720d92b66f881acd5875f56951a748f
Shariq Shaikh <shariqshaikh2705@gmail.com>
2025-04-26 22:21:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1=="" || s2==""){
+		return "";
+	}
+	else{
+		let c1=s1[0];
+		let c2=s2[0];
+		return (c1+""+c2);
+	}
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
+alert(shortcut(s1, s2));  
